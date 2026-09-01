2cc2906bd78be08575cc28590a0becaa1785f214
msaleem-temp <jetminds.ai@gmail.com>
2026-09-02 01:45:45 +0500

    Handler function testing

--- code/data/splits.py
+++ code/data/splits.py
@@ -293,11 +293,13 @@ def split_handler(
     print("=" * 60)
 
     # 9. Save Files
+    os.makedirs(output_dir, exist_ok=True)
+    
     train_path = os.path.join(output_dir, "train.json")
     val_path = os.path.join(output_dir, "val.json")
     test_path = os.path.join(output_dir, "test.json")
 
-    
+
     print(f"This is train path: {train_path}")
     with open(train_path, 'w') as f:
         json.dump(train_patches, f, indent=4)
