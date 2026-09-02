86e98fcfa317f3e4a9a3087b65b22e1a866ae2cd
msaleem-temp <jetminds.ai@gmail.com>
2026-09-02 09:52:53 +0500

    aug config test

--- code/train.py
+++ code/train.py
@@ -90,7 +90,7 @@ def main():
             target_classes=config.data.target_classes,
     )
 
-    print(config.label_map)
+    print(config.augmentation)
     print("Existed...")
     sys.exit(0)
     # 4. Build zarr map
